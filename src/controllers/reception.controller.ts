a4d1ece145b274a7d35e73d74d1ed616f4caa51e
Obkeldiyev <obkeldiyevgiga13@gmail.com>
2026-02-16 10:27:10 +0500

    one function

--- src/controllers/reception.controller.ts
+++ src/controllers/reception.controller.ts
@@ -1,5 +1,29 @@
-import { PrismaClient } from "@prisma/client";
+import { ErrorHandler } from "@errors";
+import { PrismaClient, Reception } from "@prisma/client";
+import { NextFunction, Request, Response } from "express";
 
 const client = new PrismaClient();
 
-export class ReceptionController {}
+export class ReceptionController {
+    static async getReceptions(req: Request, res: Response, next: NextFunction) {
+        try {
+            const reception: Reception[] | [] = await client.reception.findMany({
+                include: {
+                    media: true
+                }
+            });
+
+            res.status(200).send({
+                success: true, 
+                message: "All reception staff",
+                data: reception
+            })
+        } catch (error: any) {
+            next(new ErrorHandler(error.messsage, error.status))
+        }
+    }
+
+    static async createreception(req: Request, res: Response, next: NextFunction) {
+        
+    }
+}
