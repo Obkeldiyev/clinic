e6bf05d1b78a05d4573a6950478bd27bca73a98c
Obkeldiyev <obkeldiyevgiga13@gmail.com>
2026-02-19 12:44:27 +0500

    config upload for patient

---
+++ src/config/patientUploads.ts
@@ -0,0 +1,54 @@
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+import { Request } from "express";
+
+const uploadPath = path.join(__dirname, "../../uploads/patients");
+
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
+
+const storage = multer.diskStorage({
+  destination: (_req, _file, cb) => {
+    cb(null, uploadPath);
+  },
+
+  filename: (_req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const ext = path.extname(file.originalname);
+    cb(null, `patient-${uniqueSuffix}${ext}`);
+  },
+});
+
+const fileFilter: multer.Options["fileFilter"] = (
+  _req: Request,
+  file,
+  cb
+) => {
+  const allowedTypes = [
+    "image/jpeg",
+    "image/png",
+    "image/webp",
+    "image/jpg",
+    "video/mp4",
+    "video/mpeg",
+    "video/quicktime",
+    "video/x-msvideo",
+  ];
+
+  if (allowedTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only images and videos are allowed!"));
+  }
+};
+
+export const patientUpload = multer({
+  storage,
+  fileFilter,
+  limits: {
+    fileSize: 2 * 1024 * 1024 * 1024,
+    files: 15,
+  },
+});
